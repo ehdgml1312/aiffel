ce371a0a77f830d2ec4b5fe40c982d00ad9f5197
ehdgml1312 <ehdgml1312@naver.com>
2023-03-03 16:48:08 +0900

    .

---
+++ main.py
@@ -0,0 +1,35 @@
+import sys
+
+class Set():
+    def __init__(self):
+        self.list = [0]*20
+    def add(self,x):
+        if self.list[x] == 0:
+            self.list[x] = 1
+    def remove(self,x):
+        if self.list[x] == 1:
+            self.list[x] = 0
+    def check(self,x):
+        if self.list[x] == 1:
+            print(1)
+        else:
+            print(0)
+    def toggle(self,x):
+        if self.list[x] == 0:
+            self.list[x] = 1
+        else:
+            self.list[x] = 0
+    def all(self):
+        self.list = [1]*20
+    def empty(self):
+        self.list = [0]*20
+
+n = int(input())
+set = Set()
+
+for _ in range(n):
+    order = sys.stdin.readline().split()
+    if len(order) == 1:
+        getattr(set,order[0])()
+    else:
+        getattr(set,order[0])(int(order[1])-1)
